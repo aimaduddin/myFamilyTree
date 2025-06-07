fc4eb0288353ade3cd90226e45420f5cc559c217
aimaduddin <70196389+aimaduddin@users.noreply.github.com>
2025-06-07 17:08:47 +0800

    Show member and parent IDs

--- components/FamilyTree.js
+++ components/FamilyTree.js
@@ -4,17 +4,25 @@ export default function FamilyTree({ data }) {
   const buildTree = (parentId = null) => {
     return data.members
       .filter(member => member.parentId === parentId)
-      .map(member => (
-        <li key={member.id} className="mb-2">
-          <Card className="inline-block">{member.name}</Card>
-          <ul className="pl-4 mt-2">{buildTree(member.id)}</ul>
-        </li>
-      ))
+      .map(member => {
+        const children = buildTree(member.id)
+        return (
+          <li key={member.id}>
+            <Card className="node">
+              <div>{member.name}</div>
+              <div className="meta">ID: {member.id} | Parent: {member.parentId ?? 'none'}</div>
+            </Card>
+            {children.length > 0 && <ul>{children}</ul>}
+          </li>
+        )
+      })
   }
 
   return (
-    <ul>
-      {buildTree(null)}
-    </ul>
+    <div className="tree">
+      <ul>
+        {buildTree(null)}
+      </ul>
+    </div>
   )
 }
